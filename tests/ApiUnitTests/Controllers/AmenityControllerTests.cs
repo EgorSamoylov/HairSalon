370b44e588d8913a9949df2bdda7630e659b476e
Egor Samoylov <mr.egoras@mail.ru>
2025-04-08 23:22:57 +0300

    Unit тесты

---
+++ tests/ApiUnitTests/Controllers/AmenityControllerTests.cs
@@ -0,0 +1,123 @@
+﻿using Api.Controllers;
+using Application.DTOs;
+using Application.Request.AmenityRequest;
+using Application.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace ApiUnitTests.Controllers
+{
+    public class AmenityControllerTests
+    {
+        private readonly Mock<IAmenityService> _amenityServiceMock;
+        private readonly AmenityController _controller;
+
+        public AmenityControllerTests()
+        {
+            _amenityServiceMock = new Mock<IAmenityService>();
+            _controller = new AmenityController(_amenityServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnOkResult_WhenAmenityExists()
+        {
+            // Arrange
+            var amenityDto = new AmenityDTO { Id = 1, ServiceName = "Haircut" };
+            _amenityServiceMock.Setup(x => x.GetById(It.IsAny<int>()))
+                .ReturnsAsync(amenityDto);
+
+            // Act
+            var result = await _controller.GetById(1);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().BeEquivalentTo(amenityDto);
+            _amenityServiceMock.Verify(x => x.GetById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnOkResult_WithAmenitiesList()
+        {
+            // Arrange
+            var amenities = new List<AmenityDTO>
+            {
+                new AmenityDTO { Id = 1, ServiceName = "Haircut" },
+                new AmenityDTO { Id = 2, ServiceName = "Beard Trim" }
+            };
+            _amenityServiceMock.Setup(x => x.GetAll())
+                .ReturnsAsync(amenities);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().BeEquivalentTo(amenities);
+            _amenityServiceMock.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Add_ShouldReturnCreatedAtAction_WithAmenityId()
+        {
+            // Arrange
+            var request = new CreateAmenityRequest
+            {
+                ServiceName = "New Service",
+                Description = "",
+
+            };
+            var amenityId = 1;
+            _amenityServiceMock.Setup(x => x.Add(It.IsAny<CreateAmenityRequest>()))
+                .ReturnsAsync(amenityId);
+
+            // Act
+            var result = await _controller.Add(request);
+
+            // Assert
+            result.Should().BeOfType<CreatedAtActionResult>();
+            var createdAtResult = result as CreatedAtActionResult;
+            createdAtResult.ActionName.Should().Be(nameof(_controller.GetById));
+            createdAtResult.Value.Should().BeEquivalentTo(new { Id = amenityId });
+            createdAtResult.RouteValues["id"].Should().Be(amenityId);
+            _amenityServiceMock.Verify(x => x.Add(request), Times.Once);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnNoContent_WhenSuccess()
+        {
+            // Arrange
+            var request = new UpdateAmenityRequest
+            {
+                Description = "",
+                ServiceName = "Update service"
+            };
+            _amenityServiceMock.Setup(x => x.Update(It.IsAny<UpdateAmenityRequest>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.Update(request);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+            _amenityServiceMock.Verify(x => x.Update(request), Times.Once);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNoContent_WhenSuccess()
+        {
+            // Arrange
+            _amenityServiceMock.Setup(x => x.Delete(It.IsAny<int>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.Delete(1);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+            _amenityServiceMock.Verify(x => x.Delete(1), Times.Once);
+        }
+    }
+}
---
+++ tests/ApiUnitTests/Controllers/ClientControllerTests.cs
@@ -0,0 +1,156 @@
+﻿using Application.Request.ClientRequest;
+using Application.Services;
+using Application.DTOs;
+using Application.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using FluentAssertions;
+using Api.Controllers;
+using Bogus;
+using Bogus.DataSets;
+
+
+namespace ApiUnitTests.Controllers
+{
+    public class ClientControllerTests
+    {
+        private readonly Mock<IClientService> _clientServiceMock;
+        private readonly ClientController _controller;
+
+        public ClientControllerTests()
+        {
+            _clientServiceMock = new Mock<IClientService>();
+            _controller = new ClientController(_clientServiceMock.Object);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnOkResult_WhenClientExists()
+        {
+            Faker faker = new Faker();
+            // Arrange
+            var clientDto = new ClientDTO 
+            { 
+                Id = 1, 
+                FirstName = faker.Person.FirstName, 
+                LastName = faker.Person.LastName,
+                Email = faker.Person.Email,
+                PhoneNumber = faker.Person.Phone
+            };
+            _clientServiceMock.Setup(x => x.GetById(It.IsAny<int>()))
+                .ReturnsAsync(clientDto);
+
+            // Act
+            var result = await _controller.GetById(1);
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().BeEquivalentTo(clientDto);
+            _clientServiceMock.Verify(x => x.GetById(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnOkResult_WithClientsList()
+        {
+            Faker faker = new Faker();
+            // Arrange
+            var clients = new List<ClientDTO>
+            {
+                new ClientDTO
+                {
+                    Id = 1,
+                    FirstName = faker.Person.FirstName,
+                    LastName = faker.Person.LastName,
+                    Email = faker.Person.Email,
+                    PhoneNumber = faker.Person.Phone
+                },
+                new ClientDTO
+                {
+                    Id = 2,
+                    FirstName = faker.Person.FirstName,
+                    LastName = faker.Person.LastName,
+                    Email = faker.Person.Email,
+                    PhoneNumber = faker.Person.Phone
+                }
+            };
+            _clientServiceMock.Setup(x => x.GetAll())
+                .ReturnsAsync(clients);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+            var okResult = result as OkObjectResult;
+            okResult.Value.Should().BeEquivalentTo(clients);
+            _clientServiceMock.Verify(x => x.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Add_ShouldReturnCreatedAtAction_WithClientId()
+        {
+            Faker faker = new Faker();
+            // Arrange
+            var request = new CreateClientRequest
+            {
+                FirstName = faker.Person.FirstName,
+                LastName = faker.Person.LastName,
+                Email = faker.Person.Email,
+                PhoneNumber = faker.Person.Phone
+            };
+            var clientId = 1;
+            _clientServiceMock.Setup(x => x.Add(It.IsAny<CreateClientRequest>()))
+                .ReturnsAsync(clientId);
+
+            // Act
+            var result = await _controller.Add(request);
+
+            // Assert
+            result.Should().BeOfType<CreatedAtActionResult>();
+            var createdAtResult = result as CreatedAtActionResult;
+            createdAtResult.ActionName.Should().Be(nameof(_controller.GetById));
+            createdAtResult.Value.Should().BeEquivalentTo(new { Id = clientId });
+            createdAtResult.RouteValues["id"].Should().Be(clientId);
+            _clientServiceMock.Verify(x => x.Add(request), Times.Once);
+        }
+
+        [Fact]
+        public async Task Update_ShouldReturnNoContent_WhenSuccess()
+        {
+            Faker faker = new Faker();
+            // Arrange
+            var request = new UpdateClientRequest
+            {
+                ClientId = 1,
+                FirstName = faker.Person.FirstName,
+                LastName = faker.Person.LastName,
+                Email = faker.Person.Email,
+                PhoneNumber = faker.Person.Phone
+            };
+            _clientServiceMock.Setup(x => x.Update(It.IsAny<UpdateClientRequest>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.Update(request);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+            _clientServiceMock.Verify(x => x.Update(request), Times.Once);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldReturnNoContent_WhenSuccess()
+        {
+            // Arrange
+            _clientServiceMock.Setup(x => x.Delete(It.IsAny<int>()))
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _controller.Delete(1);
+
+            // Assert
+            result.Should().BeOfType<NoContentResult>();
+            _clientServiceMock.Verify(x => x.Delete(1), Times.Once);
+        }
+    }
+}
