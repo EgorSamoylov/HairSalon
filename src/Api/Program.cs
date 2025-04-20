cf2055afd1bd210d8c3a962f7410e736b46cb6d1
Egor Samoylov <mr.egoras@mail.ru>
2025-04-20 22:22:27 +0300

    Убрал лишнюю строку с комментарием

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -22,7 +22,6 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Services.AddSerilog();
 
-// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
